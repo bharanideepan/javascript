fc5a748f76740f99ca80406c4a7c2a0df62f7625
Bharani <bharani@ideas2it.com>
2022-03-12 22:42:17 +0530

    Touch listener added

--- 2048-js/script.js
+++ 2048-js/script.js
@@ -10,7 +10,52 @@ setupInput();
 
 function setupInput() {
   window.addEventListener("keydown", handleInput, { once: true });
+  document.addEventListener('touchstart', handleTouchStart, false);        
+  document.addEventListener('touchmove', handleTouchMove, false);
 }
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            handleInput({key: 'ArrowLeft'})
+        } else {
+            handleInput({key: 'ArrowRight'})
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+          handleInput({key: 'ArrowUp'})
+        } else { 
+          handleInput({key: 'ArrowDown'})
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
 
 async function handleInput(e) {
   switch (e.key) {
